30d1498218d812747ce13143c1d0aca33f4f33d5
huxiaojun01 <44152084+huxiaojun01@users.noreply.github.com>
2019-10-17 08:28:01 +0800

    哈哈哈

--- eureka/src/main/java/com/imooc/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/imooc/eureka/EurekaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaApplication {
 
     public static void main(String[] args) {
+        System.out.println(666);
         SpringApplication.run(EurekaApplication.class, args);
     }
 
